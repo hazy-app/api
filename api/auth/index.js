750c899f99295183dc026849845ec71f34af9315
Nainemom <nainemom@gmail.com>
2018-11-24 19:26:03 +0330

    cleanup

--- api/auth/index.js
+++ api/auth/index.js
@@ -5,46 +5,46 @@ const auth = require(path.resolve(__rootdir, './lib/auth.js'))
 const authModel = require(path.resolve(__rootdir, './schema/post-auth.modela.js'))
 
 module.exports = {
-  post: [async (req, res, next) => {
-    const model = new Modela(authModel)
-    model.$set(req.body).$clean()
-    const check = model.$check()
-    if (!check.result) {
-      return res.status(400).send(Object.assign(check, {
-        message: 'Bad request'
-      }))
-    }
-    req.body = model.$export()
-    next()
-  }, async (req, res) => {
-    const user = await database.getTable('users').getOne({
-      username: req.body.username
-    })
-    if (!user) {
-      return res.status(404).send({
-        result: false,
-        message: 'User not found'
+  post: [
+    async (req, res, next) => {
+      const model = new Modela(authModel)
+      model.$set(req.body).$clean()
+      const check = model.$check()
+      if (!check.result) {
+        return res.status(400).send(Object.assign(check, {
+          message: 'Bad request'
+        }))
+      }
+      req.body = model.$export()
+      next()
+    }, async (req, res) => {
+      const user = await database.getTable('users').getOne({
+        username: req.body.username
       })
-    }
+      if (!user) {
+        return res.status(404).send({
+          result: false,
+          message: 'User not found'
+        })
+      }
 
-    const check = bcrypt.compareSync(req.body.password, user.password)
-    if (!check) {
-      return res.status(401).send({
-        result: false,
-        message: 'Wrong password'
-      })
+      const check = bcrypt.compareSync(req.body.password, user.password)
+      if (!check) {
+        return res.status(401).send({
+          result: false,
+          message: 'Wrong password'
+        })
+      }
+      try {
+        const token = await auth.sign({
+          username: user.username,
+          create_date: user.create_date
+        })
+        res.send(`bearer ${token}`)
+      } catch (e) {
+        console.log(e)
+        res.status(500).send(e)
+      }
     }
-    try {
-      const token = await auth.sign({
-        username: user.username,
-        create_date: user.create_date
-      })
-      res.send(`bearer ${token}`)
-    } catch (e) {
-      console.log(e)
-      res.status(500).send(e)
-    }
-
-
-  }]
+  ]
 }
