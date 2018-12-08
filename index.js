50143a078789edfc5bfdc9d0b9298b4ab0149024
Nainemom <nainemom@gmail.com>
2018-12-08 18:12:43 +0330

    add new lib: push

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ global.BCRYPT_SALT_ROUNDS = process.env.BCRYPT_SALT_ROUNDS ? parseInt(process.en
 global.JWT_SECRET = process.env.JWT_SECRET
 global.RECAPTCHA_SITE_KEY = process.env.RECAPTCHA_SITE_KEY
 global.RECAPTCHA_SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY
+global.FIREBASE_SERVER_KEY = process.env.FIREBASE_SERVER_KEY
 
 /// MODULES
 global.__rootdir = __dirname
@@ -12,11 +13,13 @@ const express = require('express')
 const routes = require('./lib/routes.js')
 const middlewares = require('./lib/middlewares.js')
 const Database = require('./lib/Database.js')
+global.push = require('./lib/push.js')
 global.database = new Database()
 
 // APP
 const init = async () => {
   await database.connect(MONGO_URI)
+  console.log('mongo connected')
   database.setTable('users', require('./schema/users.mongo.js'))
   database.setTable('messages', require('./schema/messages.mongo.js'))
   const server = express()
