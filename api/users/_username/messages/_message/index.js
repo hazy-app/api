0a49faee6bde20132751899948dbf1c77287923b
Nainemom <nainemom@gmail.com>
2018-12-11 19:48:45 +0330

    add single message get method

--- api/users/_username/messages/_message/index.js
+++ api/users/_username/messages/_message/index.js
@@ -5,6 +5,23 @@ const bcrypt = require('bcryptjs')
 const messageModel = require(path.resolve(__rootdir, './schema/post-message.modela.js'))
 
 module.exports = {
+  get: [
+    async (req, res) => {
+      try {
+        const message = await database.getTable('messages').getOne({
+          _id: req.params.message
+        })
+        if (message.receiver.toLowerCase() !== req.params.username.toLowerCase()) {
+          throw 404
+        }
+        res.status(200).send(message)
+      } catch (e) {
+        return res.status(404).send({
+          message: 'Message not found.'
+        })  
+      }
+    }
+  ],
   delete: [
     auth.basic,
     async (req, res, next) => {
