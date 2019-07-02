d5b8ad67a1ad91e1a5ab019b8aa0540a0044e59f
Nainemom <nainemom@gmail.com>
2019-07-02 14:25:28 +0430

    add more checks

--- schema/post-poll.modela.js
+++ schema/post-poll.modela.js
@@ -5,7 +5,7 @@ module.exports = {
   choices: {
     type: ['array'],
     default: () => [],
-    validator: val => val.length >= 2,
+    validator: val => val.length >= 2 && val.length <= 8,
     message: () => 'You should have atleast 2 item in array.'
   }
 }
